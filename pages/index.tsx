7b6d3036f0f483c8881489305402be75751590ea
lucianpurcaru <[lucian.purcaru@gmail.com](mailto:lucian.purcaru@gmail.com)>
2023-02-11 15:15:01 +0100

    responsive header done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,30 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from './Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Let's build Netflix
-      {/* Header */}
+      <Header />
       <main>
-      {/* Banner */}
-      <section>
-        {/* Row */}
-        {/* Row */}
-        {/* Row */}
-        {/* Row */}
-        {/* Row */}
-        {/* Row */}
-      </section>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
       </main>
       {/* Modal */}
     </div>
   )
 }
 
-export default Home;
+export default Home
