402c7e709974ee995b739d0c150be7b6f539374f
lucianpurcaru <[lucian.purcaru@gmail.com](mailto:lucian.purcaru@gmail.com)>
2023-02-12 13:02:52 +0100

    login function done

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,33 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import useAuth from '../hooks/useAuth'
+
+interface Inputs {
+  email: string
+  password: string
+}
 
 function Login() {
+  const [login, setLogin] = useState(false)
+  const { signIn, signUp } = useAuth()
 
-    const [login, setLogin] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    console.log(data)
+    if (login) {
+      await signIn(data.email, data.password)
+    } else {
+      await signUp(data.email, data.password)
+    }
+  }
 
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
@@ -17,6 +40,7 @@ function Login() {
         layout="fill"
         className="-z-10 !hidden opacity-60 sm:!inline"
         objectFit="cover"
+        alt=""
       />
       <img
         src="https://rb.gy/ulxxee"
@@ -24,24 +48,60 @@ function Login() {
         width={150}
         height={150}
       />
-      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+
+      <form
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <h1 className="text-4xl font-semibold">Sign In</h1>
         <div className="space-y-4">
           <label className="inline-block w-full">
-            <input type="email" placeholder="Email" className="input" />
+            <input
+              type="email"
+              placeholder="Email"
+              className={`input ${
+                errors.email && 'border-b-2 border-orange-500'
+              }`}
+              {...register('email', { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
           </label>
           <label className="inline-block w-full">
-            <input type="password" placeholder="Password" className="input" />
+            <input
+              type="password"
+              {...register('password', { required: true })}
+              placeholder="Password"
+              className={`input ${
+                errors.password && 'border-b-2 border-orange-500'
+              }`}
+            />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
           </label>
         </div>
-
-        <button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+        <button
+          className="w-full rounded bg-[#E50914] py-3 font-semibold"
+          onClick={() => setLogin(true)}
+          type="submit"
+        >
           Sign In
         </button>
-
         <div className="text-[gray]">
-          New to Netflix?{''}
-          <button type="submit" className="text-withe hover:underline">Sign up now</button>
+          New to Netflix?{' '}
+          <button
+            className="cursor-pointer text-white hover:underline"
+            onClick={() => setLogin(false)}
+            type="submit"
+          >
+            Sign up now
+          </button>
         </div>
       </form>
     </div>
